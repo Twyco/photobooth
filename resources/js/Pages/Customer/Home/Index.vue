f6a0cd05e6416bb01917d9b8f77ec59a23933902
Twyco <twyco@icloud.com>
2025-03-28 12:15:01 +0100

    added container

--- resources/js/Pages/Customer/Home/Index.vue
+++ resources/js/Pages/Customer/Home/Index.vue
@@ -1,57 +1,17 @@
 <script setup lang="ts">
 import AppLayout from '@/Layouts/AppLayout.vue';
+import TitleSeparator from "@/Components/TitleSeparator.vue";
 </script>
 
 <template>
   <AppLayout>
-    <p>Hello World first</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hello World</p>
-    <p>Hallo Welt</p>
+    <div class="md:container md:mx-auto my-12 px-6 md:px-0">
+      <div class="max-w-5xl md:mx-auto">
+      <TitleSeparator title="Wie funktioniert es?"/>
+        <div class="container-small mb-6">
+
+        </div>
+      </div>
+    </div>
   </AppLayout>
 </template>
