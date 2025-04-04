b8696b17ebec6180929fb24887c070f1140d00e5
Twyco <twyco@icloud.com>
2025-04-05 00:53:36 +0200

    adapted show, index, create album to new design

---
+++ resources/js/Components/TextareaInput.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+defineProps({
+  rows: {type: Number, default: 5},
+  resize: {type: Boolean, default: false},
+});
+
+const model = defineModel<string>({ required: true });
+
+const input = ref<HTMLInputElement | null>(null);
+
+onMounted(() => {
+  if (input.value?.hasAttribute('autofocus')) {
+    input.value?.focus();
+  }
+});
+
+defineExpose({ focus: () => input.value?.focus() });
+</script>
+
+<template>
+  <textarea
+    :rows="rows"
+    class="rounded-md border-highlight shadow-sm focus:border-secondary focus:ring-secondary w-full text-primary bg-footer"
+    :class="!resize ? 'resize-none' : ''"
+    v-model="model"
+    ref="input"
+  />
+</template>
+
+<style>
+input:-webkit-autofill {
+  -webkit-box-shadow: 0 0 0 1000px theme('colors.footer') inset !important;
+  -webkit-text-fill-color: theme('colors.primary') !important;
+}
+</style>
--- resources/js/Pages/Admin/Album/Show.vue
+++ resources/js/Pages/Admin/Album/Show.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
-import { PropType } from 'vue';
-import { AdminAlbumInterface } from '@/types/album-interface';
+import {PropType} from 'vue';
+import {AdminAlbumInterface} from '@/types/album-interface';
 import AppLayout from '@/Layouts/AppLayout.vue';
+import TitleSeparator from "@/Components/TitleSeparator.vue";
+import {format} from "date-fns";
+import {Link} from "@inertiajs/vue3";
 
 defineProps({
   album: {
@@ -12,39 +15,91 @@ defineProps({
 </script>
 
 <template>
-  <AppLayout title="Album ansicht">
-    <ul>
-      <li><b>ID:</b> {{ album.id }}</li>
-      <li><b>Titel:</b> {{ album.title }}</li>
-      <li><b>Beschreibung:</b> {{ album.description }}</li>
-      <li>
-        <b>Veranstaltungsdatum:</b>
-        {{ album.eventDate }}
-      </li>
-      <li><b>UUID:</b> {{ album.uuid }}</li>
-      <li>
-        <b>Direktlink: </b>
-        <a
-          :href="route('album.show', { album: album.uuid })"
-          class="text-blue-800 visited:text-blue-800 active:text-blue-800"
+  <AppLayout :title="album.title">
+    <div class="md:container md:mx-auto my-12 px-8 md:px-0">
+      <div class="max-w-5xl md:mx-auto">
+        <Link
+          :href="route('admin.album.index')"
+          class="text-primary inline-flex ml-2 py-1 rounded-lg mb-1"
         >
-          {{ route('album.show', { album: album.uuid }) }}
-        </a>
-      </li>
-      <li v-if="album.qrCode">
-        <b>Qrcode:</b>
-        <img :src="album.qrCode" alt="Qrcode" width="200" class="ml-2" />
-      </li>
-      <li>
-        <b>Codes:</b>
-        <ul class="ml-2">
-          <li v-for="accessCode in album.accessCodes">
-            <b>Code:</b>
-            {{ accessCode.accessCode }} - <b>Usages:</b>
-            {{ accessCode.usages }} - <b>Saves:</b> {{ accessCode.saves }}
-          </li>
-        </ul>
-      </li>
-    </ul>
+          <i class="mdi mdi-arrow-left"/> Zurück zur Übersicht
+        </Link>
+        <TitleSeparator title="Albuminfo"/>
+        <div class="hidden lg:flex gap-x-2 px-8 text-primary mb-2">
+          <div class="flex flex-col flex-1 bg-footer px-4 py-2 rounded-lg gap-y-2">
+            <div class="flex gap-2">
+              <span class="font-bold shrink-0">ID:</span>
+              <p class="text-justify">{{ album.id }}</p>
+            </div>
+            <div class="flex gap-2">
+              <span class="font-bold shrink-0">Titel:</span>
+              <p class="text-justify">{{ album.title }}</p>
+            </div>
+            <div class="flex gap-2">
+              <span class="font-bold shrink-0">Datum der Veranstaltung:</span>
+              <p class="text-justify">{{ format(new Date(album.eventDate), 'dd.MM.yyyy') }}</p>
+            </div>
+            <div class="flex gap-2">
+              <span class="font-bold shrink-0">UUID:</span>
+              <p class="text-justify">{{ album.uuid }}</p>
+            </div>
+            <div class="flex gap-2">
+              <span class="font-bold shrink-0">Cover:</span>
+              <img
+                class="aspect-square w-1/2"
+                :src="album.cover  ?? '/images/no_cover_fallback.webp'"
+                alt="Albumcover"
+              />
+            </div>
+          </div>
+          <div class="flex flex-col flex-1 bg-footer px-4 py-2 rounded-lg gap-y-2">
+            <div class="flex gap-2">
+              <span class="font-bold shrink-0">Beschreibung:</span>
+              <p class="text-justify">{{ album.description }}</p>
+            </div>
+            <div class="flex gap-2">
+              <span class="font-bold shrink-0">Direktzugang:</span>
+              <img
+                class="aspect-square w-1/2"
+                :src="album.qrCode"
+                alt="QrCode"
+              />
+            </div>
+          </div>
+        </div>
+        <!--Mobile View-->
+        <div class="grid grid-cols-[auto_1fr] lg:hidden bg-footer gap-y-2 gap-x-2 rounded-lg p-2 text-primary mb-2">
+          <span class="font-bold">ID:</span>
+          <p>{{ album.id }}</p>
+
+          <span class="font-bold">Titel:</span>
+          <p>{{ album.title }}</p>
+
+          <span class="font-bold">Datum der Veranstaltung:</span>
+          <p>{{ format(new Date(album.eventDate), 'dd.MM.yyyy') }}</p>
+
+          <span class="font-bold">Beschreibung:</span>
+          <p>{{ album.description }}</p>
+
+          <span>UUID:</span>
+          <p>{{ album.uuid }}</p>
+
+          <span class="font-bold">Direktzugang:</span>
+          <img
+            class="aspect-square w-full"
+            :src="album.qrCode"
+            alt="QrCode"
+          />
+
+          <span class="font-bold">Cover:</span>
+          <img
+            class="aspect-square w-full"
+            :src="album.cover  ?? '/images/no_cover_fallback.webp'"
+            alt="Albumcover"
+          />
+
+        </div>
+      </div>
+    </div>
   </AppLayout>
 </template>
