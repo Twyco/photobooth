233da40c44c925a90615197c05a184d6ca15e837
Twyco <twyco@icloud.com>
2025-04-13 12:30:00 +0200

    new design to forgot password, verify email Page

--- resources/js/Pages/Auth/VerifyEmail.vue
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
-import { Head, Link, useForm } from '@inertiajs/vue3';
+import { Link, useForm } from '@inertiajs/vue3';
 import AppLayout from '@/Layouts/AppLayout.vue';
+import TitleSeparator from "@/Components/TitleSeparator.vue";
 
 const props = defineProps<{
   status?: string;
@@ -20,40 +21,45 @@ const verificationLinkSent = computed(
 </script>
 
 <template>
-  <AppLayout>
-    <Head title="Email Verification" />
+  <AppLayout title="Email bestätigen">
+    <div class="md:container md:mx-auto my-12 px-6 md:px-0">
+      <div class="max-w-5xl md:mx-auto">
+        <div class="max-w-lg mx-auto mt-12">
+        <TitleSeparator title="Danke fürs Registrieren" />
 
-    <div class="mb-4 text-sm text-gray-600">
-      Thanks for signing up! Before getting started, could you verify your email
-      address by clicking on the link we just emailed to you? If you didn't
-      receive the email, we will gladly send you another.
-    </div>
+          <div class="mb-4 text-base text-primary">
+            Bevor du loslegen kannst, bestätige bitte deine E-Mail-Adresse über den Link,
+            den wir dir gerade geschickt haben.
+          </div>
 
-    <div
-      class="mb-4 text-sm font-medium text-green-600"
-      v-if="verificationLinkSent"
-    >
-      A new verification link has been sent to the email address you provided
-      during registration.
-    </div>
+          <div
+            class="mb-4 text-sm font-medium text-green-600"
+            v-if="verificationLinkSent"
+          >
+            Wir haben dir gerade einen neuen Bestätigungslink an deine E-Mail geschickt,
+            falls du keine E-Mail erhalten hast, überprüfe auch deinen Spam-Ordner.
+          </div>
 
-    <form @submit.prevent="submit">
-      <div class="mt-4 flex items-center justify-between">
-        <PrimaryButton
-          :class="{ 'opacity-25': form.processing }"
-          :disabled="form.processing"
-        >
-          Resend Verification Email
-        </PrimaryButton>
+          <form @submit.prevent="submit">
+            <div class="mt-4 flex items-center justify-between">
+              <PrimaryButton
+                :class="{ 'opacity-25': form.processing }"
+                :disabled="form.processing"
+              >
+                Bestätigungs-Email erneut senden
+              </PrimaryButton>
 
-        <Link
-          :href="route('logout')"
-          method="post"
-          as="button"
-          class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          >Log Out</Link
-        >
+              <Link
+                :href="route('logout')"
+                method="post"
+                as="button"
+                class="rounded-md text-sm text-primary underline hover:text-secondary focus:outline-none focus:ring-2 focus:ring-secondary focus:ring-offset-2"
+                >Ausloggen</Link
+              >
+            </div>
+          </form>
+        </div>
       </div>
-    </form>
+    </div>
   </AppLayout>
 </template>
