1741f4757031b2b1b11691267b398da271bea762
Twyco <twyco@icloud.com>
2025-04-19 01:10:04 +0200

    mobile view fixes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
 
         <title inertia>{{ config('app.name', 'Fotobox') }}</title>
 
@@ -17,7 +17,7 @@
         @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased overflow-y-auto" style="background: #131314">
+    <body class="font-sans antialiased" style="background: #131314">
         @inertia
     </body>
 </html>
