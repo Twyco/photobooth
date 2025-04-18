847b372be72729219753ab3502abb2ec568808bc
Twyco <twyco@icloud.com>
2025-04-18 22:24:56 +0200

    symlink fix

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@ Route::middleware([EnsureEmailIsVerifiedIfAuthenticated::class])->group(function
     Route::get('/', fn () => Inertia::render('Customer/Home/Index'))->name('home');
     Route::get('/alben', [AlbumController::class, 'index'])->name('album.index');
     Route::post('/accessCode', [AlbumAccessCodeController::class, 'use'])->name('accessCode.use');
-    Route::get('album/{album:uuid}', [AlbumController::class, 'show'])->name('album.show');
-    Route::get('album/{album:uuid}/{imageNumber}', [AlbumController::class, 'showImage'])->name('album.image.show');
+    Route::get('album-view/{album:uuid}', [AlbumController::class, 'show'])->name('album.show');
+    Route::get('album-view/{album:uuid}/{imageNumber}', [AlbumController::class, 'showImage'])->name('album.image.show');
 });
 
 Route::middleware(['auth'])->group(function () {
@@ -21,7 +21,7 @@ Route::middleware(['auth'])->group(function () {
   Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
   Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-  Route::middleware([EnsureEmailIsVerifiedIfAuthenticated::class])->post('album/{album:uuid}/save', [AlbumController::class, 'save'])->name('album.save');
+  Route::middleware([EnsureEmailIsVerifiedIfAuthenticated::class])->post('album-view/{album:uuid}/save', [AlbumController::class, 'save'])->name('album.save');
 });
 
 require __DIR__ . '/auth.php';
