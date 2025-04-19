df75c96270600a87a87e886774d23bb88ab2f9a8
Twyco <twyco@icloud.com>
2025-04-19 13:30:49 +0200

    test

--- routes/commands.php
+++ routes/commands.php
@@ -3,4 +3,4 @@
 use App\Console\Commands\DeleteUnverifiedUsersCommand;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command(DeleteUnverifiedUsersCommand::class)->everyThreeMinutes();
+Schedule::command(DeleteUnverifiedUsersCommand::class)->everyMinute();
