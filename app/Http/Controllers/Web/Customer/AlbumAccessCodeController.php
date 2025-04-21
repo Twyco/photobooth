2b3c16022177adfecdcd04cc9e4564530bbb00a9
Twyco <twyco@icloud.com>
2025-04-21 21:17:20 +0200

    cache bug hotfix

--- app/Http/Controllers/Web/Customer/AlbumAccessCodeController.php
+++ app/Http/Controllers/Web/Customer/AlbumAccessCodeController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\AlbumAccessCode;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
@@ -13,57 +14,58 @@ use Illuminate\Validation\ValidationException;
 class AlbumAccessCodeController extends Controller
 {
 
-  /**
-   * Store a newly created resource in storage.
-   * @throws ValidationException
-   */
-  public function store(Request $request)
-  {
-    $validatedData = Validator::make($request->all(), [
-      'albumId' => ['required', 'integer', 'exists:albums,id'],
-    ])->validateWithBag('createAlbumAccessCode');
-    AlbumAccessCode::create(['album_id' => Arr::get($validatedData, 'albumId')]);
-  }
-
-  /**
-   * Remove the specified resource from storage.
-   */
-  public function destroy(AlbumAccessCode $albumAccessCode)
-  {
-    if (!Gate::allows('delete', $albumAccessCode)) {
-      abort(403);
-    }
-    $albumAccessCode->delete();
-  }
-
-  /**
-   * @throws ValidationException
-   */
-  public function use(Request $request)
-  {
-    $validatedData = Validator::make($request->all(), [
-      'accessCode' => ['required', 'string'],
-    ])->validateWithBag('useAlbumAccessCode');
-
-    $cleanedCode = str_replace('-', '', Arr::get($validatedData, 'accessCode'));
-
-    Validator::make([
-      'accessCode' => $cleanedCode,
-    ], [
-      'accessCode' => ['exists:album_access_codes,access_code'],
-    ])->validateWithBag('useAlbumAccessCode');
-
-    $albumAccessCode = AlbumAccessCode::where('access_code', $cleanedCode)->firstOrFail();
-
-    $albumAccessCode->increment('usages');
-    $album = $albumAccessCode->album;
-    if(auth()->check()){
-      $user = $request->user();
-      $user->savedAlbums()->syncWithoutDetaching([$album->id]);
-      $albumAccessCode->increment('saves');
+    /**
+     * Store a newly created resource in storage.
+     * @throws ValidationException
+     */
+    public function store(Request $request)
+    {
+        $validatedData = Validator::make($request->all(), [
+            'albumId' => ['required', 'integer', 'exists:albums,id'],
+        ])->validateWithBag('createAlbumAccessCode');
+        AlbumAccessCode::create(['album_id' => Arr::get($validatedData, 'albumId')]);
     }
 
-    return to_route('album.show', ['album' => $album]);
-  }
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AlbumAccessCode $albumAccessCode)
+    {
+        if (!Gate::allows('delete', $albumAccessCode)) {
+            abort(403);
+        }
+        $albumAccessCode->delete();
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    public function use(Request $request)
+    {
+        $validatedData = Validator::make($request->all(), [
+            'accessCode' => ['required', 'string'],
+        ])->validateWithBag('useAlbumAccessCode');
+
+        $cleanedCode = str_replace('-', '', Arr::get($validatedData, 'accessCode'));
+
+        Validator::make([
+            'accessCode' => $cleanedCode,
+        ], [
+            'accessCode' => ['exists:album_access_codes,access_code'],
+        ])->validateWithBag('useAlbumAccessCode');
+
+        $albumAccessCode = AlbumAccessCode::where('access_code', $cleanedCode)->firstOrFail();
+
+        $albumAccessCode->increment('usages');
+        $album = $albumAccessCode->album;
+        if (auth()->check()) {
+            $user = $request->user();
+            Cache::forget($user->id . '_viewable_albums');
+            $user->savedAlbums()->syncWithoutDetaching([$album->id]);
+            $albumAccessCode->increment('saves');
+        }
+
+        return to_route('album.show', ['album' => $album]);
+    }
 
 }
